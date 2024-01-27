91916a9bf30a401413aed4c859695070ec6b1069
Pedro Arthur <pedro13arthur@gmail.com>
2024-01-27 20:30:46 -0300

    refatorar teste e2e

--- tests/e2e/task.spec.js
+++ tests/e2e/task.spec.js
@@ -1,52 +1,44 @@
 const { test, expect } = require("@playwright/test");
 
 test("should add and delete tasks", async ({ page }) => {
-  await page.goto("http://localhost:8080/");
+  const baseURL = "http://localhost:8080/";
+  await page.goto(baseURL);
 
-  // add first task
-  const addButtonFirst = await page.waitForSelector('button[id="add-button"]');
-  await addButtonFirst.click();
+  const clickIfExists = async (selector) => {
+    const element = await page.$(selector);
+    if (element) {
+      await element.click();
+    } else {
+      throw new Error(`Element with selector ${selector} not found.`);
+    }
+  };
 
-  await page.type('input[type="text"]', "Teste");
-  await page.type('input[type="date"]', "14-01-2022");
-  await page.type('input[type="time"]', "11:00");
+  const addTask = async (description, date, time) => {
+    await clickIfExists('button[id="add-button"]');
 
-  const submitButtonFirst = await page.$('button[type="submit"]');
-  await submitButtonFirst.click();
+    await page.type('input[type="text"]', description);
+    await page.type('input[type="date"]', date);
+    await page.type('input[type="time"]', time);
 
-  // add second task
-  const addButtonSecond = await page.waitForSelector('button[id="add-button"]');
-  await addButtonSecond.click();
+    await clickIfExists('button[type="submit"]');
+  };
 
-  await page.type('input[type="text"]', "Teste 2");
-  await page.type('input[type="date"]', "18-05-2023");
-  await page.type('input[type="time"]', "08:00");
+  await addTask("Teste", "2022-01-14", "11:00");
+  await addTask("Teste 2", "2023-05-18", "08:00");
 
-  const submitButtonSecond = await page.$('button[type="submit"]');
-  await submitButtonSecond.click();
-
-  // verify taskCards
   const taskCards = await page.$$("#task-card");
 
   expect(taskCards.length).toBe(2);
 
-  const hasFirstH3Element = await taskCards[0].$eval(
-    "h3",
-    (h3Element, targetText) => {
-      return h3Element.textContent.includes(targetText);
-    },
-    "Teste"
-  );
+  const taskDescriptions = ["Teste", "Teste 2"];
 
-  expect(hasFirstH3Element).toBeTruthy();
+  for (let i = 0; i < taskCards.length; i++) {
+    const hasH3Element = await taskCards[i].$eval(
+      "h3",
+      (h3Element, targetText) => h3Element.textContent.includes(targetText),
+      taskDescriptions[i]
+    );
 
-  const hasSecondH3Element = await taskCards[1].$eval(
-    "h3",
-    (h3Element, targetText) => {
-      return h3Element.textContent.includes(targetText);
-    },
-    "Teste 2"
-  );
-
-  expect(hasSecondH3Element).toBeTruthy();
+    expect(hasH3Element).toBeTruthy();
+  }
 });
